76d2866715259b1c8420b4bd323b229b6c1bea3b
aaronhans <aaronhans@gmail.com>
2021-04-24 06:28:07 -0700

    fix posts list, add markdown support

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
+// const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.htmlTemplateEngine = "njk";
+
+  eleventyConfig.addCollection("mySort", function(collection) {
+    let posts = [];
+    collection.getAll().forEach( (item) => {
+      if(item.data.tags && item.data.tags[0] == 'news') {
+        posts.push(item);
+      }
+    })
+    return posts.sort(function(a, b) {
+      return new Date(a.data.publishdate) - new Date(b.data.publishdate);
+    }).reverse();
+  });
+  // eleventyConfig.addPlugin(pluginRss);
+
   return {
     htmlTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "11ty.js"],
+    markdownTemplateEngine: "md",
+    templateFormats: ["html", "njk", "11ty.js", "md"],
     dir: {
       input: "pages",
       output: "docs",
