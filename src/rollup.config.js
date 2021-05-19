92c3f3a4191dab9b79fde3446467e5bcdcc0772a
aaronhans <aaronhans@gmail.com>
2021-05-19 16:01:49 -0700

    add javascript build

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/js/index.js',
+  input: 'src/index.js',
   output: {
     file: '_site/js/built.js',
     format: 'esm'
