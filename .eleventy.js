8e61044497a27e3dadc77d25be4998a3ff79e99a
carterm <carter.medlin@gmail.com>
2021-06-14 11:05:44 -0700

    first pass building wordpress posts

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,39 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.htmlTemplateEngine = "njk";
 
+  //Process wordpress posts
+  eleventyConfig.addCollection("wordpressposts", function(collection) {
+    const FolderName = 'wordpress-posts';
+    let output = [];
+    
+    collection.getAll().forEach(item => {
+        if(item.inputPath.includes(FolderName)) {
+          item.outputPath = item.outputPath.replace(`/${FolderName}`,'');;
+          item.url = item.url.replace(`/${FolderName}`,'');
+          item.data.page.url = item.url;
+
+          //content pulled in from JSON
+          const jsonData = item.data.data;
+          item.data.layout = "page";
+          item.data.tags = ['news'];
+          item.data.title = jsonData.title;
+          item.data.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
+          item.data.meta = jsonData.excerpt;
+          item.data.description = jsonData.excerpt;
+          item.data.lead = jsonData.excerpt;
+          item.data.author = jsonData.author;
+        } else {
+          //console.log(item.data)
+          //console.log(item);
+
+        };
+    });
+
+    return output;
+  });
+
+
+
   eleventyConfig.addCollection("mySort", function(collection) {
     let posts = [];
     collection.getAll().forEach( (item) => {
@@ -22,7 +55,7 @@ module.exports = function(eleventyConfig) {
     templateFormats: ["html", "njk", "11ty.js", "md"],
     dir: {
       input: "pages",
-      output: "docs",
+      output: "_site",
     }
   };
 }
