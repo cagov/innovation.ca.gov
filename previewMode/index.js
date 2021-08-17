b1f6b7d2791e38edd58ead5c24b1fc1f45177290
Carter Medlin <carterm@users.noreply.github.com>
2021-08-17 12:04:33 -0700

    t1

--- previewMode/index.js
+++ previewMode/index.js
@@ -9,7 +9,7 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = runCode('/',req.query);
+    context.res = {body:'test'}; //runCode('/',req.query);
 
 
 }
