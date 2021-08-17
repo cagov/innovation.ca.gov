c7be3f3ab7c2676e607959f045acd77006768de9
Carter Medlin <carterm@users.noreply.github.com>
2021-08-17 15:07:38 -0700

    fixed func call

--- previewMode/index.js
+++ previewMode/index.js
@@ -1,16 +1,16 @@
 
 const { EleventyServerless } = require("@11ty/eleventy");
-const handler = require("./possum");
+const possum = require("./possum");
 
 module.exports = async function (context, req) {
 try {
 
 
-    console.log(handler);
+    console.log(possum.handler);
 
 
     
-    const result = await handler({path:'.',queryStringParameters:req.query});
+    const result = await possum.handler({path:'.',queryStringParameters:req.query});
 
 
     context.res = {
