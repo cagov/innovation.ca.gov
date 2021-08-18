4ab49862b43c612cd554af0e4eda5e70934a288c
Carter Medlin <carterm@users.noreply.github.com>
2021-08-17 17:13:38 -0700

    root call support

--- previewMode/index.js
+++ previewMode/index.js
@@ -5,6 +5,8 @@ const possum = require("./possum");
 module.exports = async function (context, req) {
 try {
 
+  /*
+  //Spit out debug info
   context.res = {
     statusCode: 200,
     body: JSON.stringify(
@@ -18,22 +20,16 @@ try {
   };
 
   return;
-
-
-
+*/
 
   const reqPath = req.headers["x-original-url"];
 
-
-
-
-
-
-  if(req.query.previewMode) {
-    context.res = await possum.handler({path:'/previewMode',queryStringParameters:req.query});
-  } else {
+  if(req.params.segments) { // Resource call
     context.res = { status: 307, headers: { location: `https://digital.ca.gov${reqPath}` }, body: null};
+  } else {  // Root call
+    context.res = await possum.handler({path:'/previewMode',queryStringParameters:req.query});
   }
+
 } catch (error) {
     context.res = {
         statusCode: error.httpStatusCode || 500,
