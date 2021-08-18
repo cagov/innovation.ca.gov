53b420c70ac90d80c17029a114d9595d4cfededa
Carter Medlin <carterm@users.noreply.github.com>
2021-08-17 17:06:17 -0700

    checking params

--- previewMode/index.js
+++ previewMode/index.js
@@ -4,10 +4,35 @@ const possum = require("./possum");
 
 module.exports = async function (context, req) {
 try {
+
+  context.res = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        context,
+        req
+      },
+      null,
+      2
+    ),
+  };
+
+  return;
+
+
+
+
+  const reqPath = req.headers["x-original-url"];
+
+
+
+
+
+
   if(req.query.previewMode) {
     context.res = await possum.handler({path:'/previewMode',queryStringParameters:req.query});
   } else {
-    context.res = { status: 307, headers: { location: `https://digital.ca.gov${req.headers["x-original-url"]}` }, body: null};
+    context.res = { status: 307, headers: { location: `https://digital.ca.gov${reqPath}` }, body: null};
   }
 } catch (error) {
     context.res = {
