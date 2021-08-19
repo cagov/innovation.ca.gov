d720957bc89c6d75bb7f58a358021f2922da8500
Carter Medlin <carterm@users.noreply.github.com>
2021-08-19 14:52:04 -0700

    dynamic link

--- pages/previewModePage.11ty.js
+++ pages/previewModePage.11ty.js
@@ -1,16 +1,18 @@
+const fetch = require('sync-fetch');
+
 class previewModePageClass {
     // or `async data() {`
     // or `get data() {`
     data() {
         return {
             layout: "page",
-            title: "DYNAMIC Why Alpha.CA.gov is prioritizing APIs",
-            meta: "How Alpha.CA.gov is making data more accessible and useful",
-            previewimage: "img/thumb/APIs-Blog-Postman-Screenshot-1.jpg",
-            lead: "Making data more accessible and useful",
-            author: "JP Petrucione",
-            publishdate: "2020-02-21",
             tags: ["news"],
+            //title: "DYNAMIC Why Alpha.CA.gov is prioritizing APIs",
+            //meta: "How Alpha.CA.gov is making data more accessible and useful",
+            previewimage: "img/thumb/APIs-Blog-Postman-Screenshot-1.jpg",
+            //lead: "Making data more accessible and useful",
+            //author: "JP Petrucione",
+            //publishdate: "2020-02-21",
             permalink: {
                 possum: "/previewModePage"
             }
@@ -18,8 +20,30 @@ class previewModePageClass {
     }
 
     render(myData) {
-        myData.title = 'AddingMORE'+myData.title;
-        return `<p>DYNAMIC version of ${myData.title}</p><p>Q = ${JSON.stringify(myData.eleventy.serverless.query,null,2)}</p>`;
+        let wpApiPage = 'https://as-go-covid19-d-001.azurewebsites.net/wp-json/wp/v2/posts/13211?_embed=author,wp:term';
+        if (myData.eleventy.serverless.query && myData.eleventy.serverless.query.link) {
+            wpApiPage = link;
+        }
+
+
+        const response = fetch(wpApiPage);
+        const jsonData = response.json();
+
+        MapWpToData(myData,jsonData)
+        return jsonData.content.rendered;
+
+        //return `<p>DYNAMIC version of ${myData.title}</p><p>Q = ${JSON.stringify(myData.eleventy.serverless.query,null,2)}</p>`;
     }
 }
+
+const MapWpToData = (itemData,jsonData) => {
+    //content pulled in from JSON
+    itemData.title = jsonData.title.rendered;
+    itemData.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
+    itemData.meta = jsonData.excerpt.rendered;
+    itemData.description = jsonData.excerpt.rendered;
+    itemData.lead = jsonData.excerpt.rendered;
+    itemData.author = jsonData._embedded.author[0].name;
+};
+
 module.exports = previewModePageClass;
