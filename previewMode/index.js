277dc5d642c7950ebe1d47479b352703f5dcf20d
Carter Medlin <carterm@users.noreply.github.com>
2021-08-23 11:17:00 -0700

    dependency reduction

--- previewMode/index.js
+++ previewMode/index.js
@@ -1,5 +1,3 @@
-
-const { EleventyServerless } = require("@11ty/eleventy");
 const possum = require("../possum");
 
 /**
@@ -9,27 +7,9 @@ const possum = require("../possum");
  */
 module.exports = async function (context, req) {
   try {
-  /*
-    //Spit out debug info
-    context.res = {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          context,
-          req
-        },
-        null,
-        2
-      ),
-    };
-
-    return;
-  */
     if(req.params.segments) { // Resource call
       context.res = { status: 301, headers: { location: `https://digital.ca.gov${req.headers["x-original-url"]}` }, body: null};
     } else {  // Root call
-      //context.res = await possum.handler({path:'/previewMode/MyID/',queryStringParameters:req.query});
-      //context.res = await possum.handler({path:'/previewMode',queryStringParameters:{id:'hardcodedtest'}});
       context.res = await possum.handler({path:'/previewModePage',queryStringParameters:req.query});
     }
 
