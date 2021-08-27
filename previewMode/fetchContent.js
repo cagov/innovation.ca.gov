5435ae93a4211580aa22b1fe4a55025acf59df27
Carter Medlin <carterm@users.noreply.github.com>
2021-08-27 12:57:20 -0700

    cachebust

--- previewMode/fetchContent.js
+++ previewMode/fetchContent.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 const reuse = require("./reuse.json");
 
 const getPageJsonFromWordpress = async itemData => {
-    let wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/${itemData.eleventy.serverless.query.postid}?_embed`;
+    let wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/${itemData.eleventy.serverless.query.postid}?_embed&cachebust=${Math.random()}`;
 
     return await fetch(wpApiPage).then(result => result.json());
 }
 
 const getPreviewPostIds = async () => {
-    const wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/?tags=${reuse.config.previewWordPressTagId}&orderby=modified&_fields=title,modified,id`;
+    const wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/?tags=${reuse.config.previewWordPressTagId}&orderby=modified&_fields=title,modified,id&cachebust=${Math.random()}`;
     /** @type {{id:number,title:{rendered:string},modified:string}[]} */
     const result = await fetch(wpApiPage).then(result => result.json())
 
