0ebf932c6f82f2fc5ecd7dbc0c9871c24ad0d621
Carter Medlin <carterm@users.noreply.github.com>
2021-08-30 15:12:40 -0700

    TSCheck an docs

--- previewMode/11tyPage.js
+++ previewMode/11tyPage.js
@@ -1,17 +1,13 @@
+//@ts-check
 const reuse = require("./reuse.json");
-const { getPageJsonFromWordpress } = require("./fetchContent");
+const { getPostJsonFromWordpress } = require("./fetchContent");
 
 class previewModePageClass {
     async data() {
         return {
             layout: "page",
             tags: ["news"],
-            //title: "DYNAMIC Why Alpha.CA.gov is prioritizing APIs",
-            //meta: "How Alpha.CA.gov is making data more accessible and useful",
             previewimage: "img/thumb/APIs-Blog-Postman-Screenshot-1.jpg",
-            //lead: "Making data more accessible and useful",
-            //author: "JP Petrucione",
-            //publishdate: "2020-02-21",
             permalink: {
                 [reuse.config.serverlessFunctionName]: reuse.config.pagePath
             }
@@ -19,9 +15,8 @@ class previewModePageClass {
     }
 
     async render(itemData) {
-        const jsonData = await getPageJsonFromWordpress(itemData);
+        const jsonData = await getPostJsonFromWordpress(itemData);
 
-        //content pulled in from JSON
         itemData.title = jsonData.title.rendered;
         itemData.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
         itemData.meta = jsonData.excerpt.rendered;
@@ -29,7 +24,7 @@ class previewModePageClass {
         itemData.lead = jsonData.excerpt.rendered;
         itemData.author = jsonData._embedded.author[0].name;
 
-        let featuredMedia = jsonData._embedded['wp:featuredmedia'];
+        let featuredMedia = jsonData._embedded["wp:featuredmedia"];
         if (featuredMedia) {
             itemData.previewimage = featuredMedia[0].source_url;
         }
--- previewMode/fetchContent.js
+++ previewMode/fetchContent.js
@@ -1,21 +1,52 @@
-const fetch = require('node-fetch');
+//@ts-check
+const fetch = require('node-fetch/lib');
 const reuse = require("./reuse.json");
+/**
+* @typedef {Object} WordpressPostRow Expected POST input when using the Wordpress API - https://developer.wordpress.org/rest-api/reference/posts/
+* @property {number} author
+* @property {number[]} categories
+* @property {string} comment_status "closed"
+* @property {{rendered:string}} content
+* @property {string} date
+* @property {string} date_gmt
+* @property {{rendered:string}} excerpt
+* @property {number} featured_media
+* @property {string} format
+* @property {{rendered:string}} guid
+* @property {number} id
+* @property {string} link
+* @property {[]} meta
+* @property {string} modified
+* @property {string} modified_gmt
+* @property {string} ping_status "closed"
+* @property {string} slug
+* @property {string} status "publish"
+* @property {boolean} sticky
+* @property {number[]} tags
+* @property {string} template
+* @property {{rendered:string}} title
+* @property {string} type "post"
+* @property {{"wp:featuredmedia":{source_url:string}[],author:{name:string}[]}} _embedded
+* @property {*} _links
+*/
 
-const getPageJsonFromWordpress = async itemData => {
-    let wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/${itemData.eleventy.serverless.query.postid}?_embed&cachebust=${Math.random()}`;
+const getPostJsonFromWordpress = async (/** @type {{ eleventy: { serverless: { query: { postid: any; }; }; }; }} */ itemData) => {
+    let wpApiPage = reuse.config.wordPressSite + `/wp-json/wp/v2/posts/${itemData.eleventy.serverless.query.postid}?_embed&cachebust=${Math.random()}`;
 
-    return await fetch(wpApiPage).then(result => result.json());
+    /** @type {WordpressPostRow} */
+    return (await fetch(wpApiPage)
+        .then(result => result.json()));
 }
 
 const getPreviewPostIds = async () => {
-    const wpApiPage = reuse.config.wordPressSite+`/wp-json/wp/v2/posts/?tags=${reuse.config.previewWordPressTagId}&orderby=modified&_fields=title,modified,id&cachebust=${Math.random()}`;
+    const wpApiPage = reuse.config.wordPressSite + `/wp-json/wp/v2/posts/?tags=${reuse.config.previewWordPressTagId}&orderby=modified&_fields=title,modified,id&cachebust=${Math.random()}`;
+    
     /** @type {{id:number,title:{rendered:string},modified:string}[]} */
-    const result = await fetch(wpApiPage).then(result => result.json())
-
-    return result;
+    return (await fetch(wpApiPage)
+        .then(result => result.json()));
 }
 
 module.exports = {
-    getPageJsonFromWordpress,
+    getPostJsonFromWordpress,
     getPreviewPostIds
 }
