c2ae701ebb6b31720cd670454545faa63119c727
Carter Medlin <carterm@users.noreply.github.com>
2021-09-02 10:30:35 -0700

    url path fix

--- previewModeModule/serverlessHandler.js
+++ previewModeModule/serverlessHandler.js
@@ -1,6 +1,7 @@
 const constants = require("./constants.json");
-const serverlessFolder = require("../" + constants.config.serverlessFunctionName);
-
+const path = require('path'); //Path Resolve needed to make plugin mode copy work
+const xpath = path.resolve(".", constants.config.serverlessFunctionName) ;
+const serverlessFolder = require(xpath);
 
 const serverlessHandler = async (queryStringParameters) => 
      serverlessFolder.handler({ path: constants.config.pagePath, queryStringParameters });
