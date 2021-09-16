015f8380439c20059866bc0c9e74b4b9c1bc8224
Carter Medlin <carterm@users.noreply.github.com>
2021-09-16 15:47:18 -0700

    using new package for redirects

--- previewMode/index.js
+++ previewMode/index.js
@@ -2,55 +2,11 @@
 
 const { azureFunctionHandler, serverlessHandler } = require("@cagov/11ty-serverless-preview-mode");
 const fetch = require('node-fetch/lib');
-const { Response } = require('node-fetch');
+
 /**
  * Azure Function to render a single 11ty page
  * @param {{req:{headers:*,query:*},res:{statusCode:number;body:string;headers?:*};done:function}} context Azure Function context
  */
 module.exports = async function (context) {
-  const TEMPazureFunctionHandler = async (context, resourceUrl) => {
-    const req = context.req;
-    const originalUrl = req.headers["x-original-url"];
-    try {
-        if (req.query.postid || originalUrl === '/') {
-            context.res = await serverlessHandler(req.query);
-        } else { // Resource call, redirect back to the main site
-            /** @type {Response} */
-            const fetchResponse = await fetch(`${resourceUrl}${originalUrl}`);
-            if (!fetchResponse.ok) {
-              let err = new Error(`${fetchResponse.status} - ${fetchResponse.statusText} - ${fetchResponse.url}`);
-              err.httpStatusCode = fetchResponse.status;
-              throw err;
-            }
-            const body = new Uint8Array(await fetchResponse.arrayBuffer());
-            //const body = await fetchResponse.text();
-            context.res = {
-                isRaw: true,
-                headers: { 
-                    "content-type": fetchResponse.headers.get('content-type')
-                },
-                body
-            };
-            
-            //context.res.raw(new Uint8Array(buf))
-            //const data = fetchResponse.json();
-
-            //context.res = { statusCode: 301, headers: { location: `${resourceUrl}${originalUrl}` }, body: null };
-        }
-    } catch (error) {
-        context.res = {
-            statusCode: error.httpStatusCode || 500,
-            body: JSON.stringify(
-                {
-                    error: error.message,
-                },
-                null,
-                2
-            ),
-        };
-    }
-    if (context.done) context.done();
-}
-await TEMPazureFunctionHandler(context, "https://digital.ca.gov");
-  //await azureFunctionHandler(context, "https://digital.ca.gov");
+  await azureFunctionHandler(context, "https://digital.ca.gov");
 }
