b194318cbd4d2a81b2f32696ba66290eba4c4e5b
Carter Medlin <carterm@users.noreply.github.com>
2021-09-20 09:58:28 -0700

    fixing accidental deployment

--- .eleventy.js
+++ .eleventy.js
@@ -2,51 +2,56 @@ const moment = require('moment-timezone');
 // const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 //Replaces content to rendered
-const replaceContent = (item,searchValue,replaceValue) => {
+const replaceContent = (item, searchValue, replaceValue) => {
   item.template.frontMatter.content = item.template.frontMatter.content
-    .replace(searchValue,replaceValue);
+    .replace(searchValue, replaceValue);
 }
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig 
  */
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.htmlTemplateEngine = "njk";
   const wordpressImagePath = 'img/wordpress';
 
-  eleventyConfig.addPassthroughCopy({ "wordpress/media":wordpressImagePath });
+  eleventyConfig.addPassthroughCopy({ "wordpress/media": wordpressImagePath });
 
-  eleventyConfig.addFilter("dateformat", function(dateIn) {
+  eleventyConfig.addFilter("dateformat", function (dateIn) {
     return moment(dateIn).tz('America/Los_Angeles').format('M/D/YYYY');
   });
 
   //Process wordpress posts
-  eleventyConfig.addCollection("wordpressposts", function(collection) {
+  eleventyConfig.addCollection("wordpressposts", function (collection) {
     const FolderName = 'wordpress-posts';
     let output = [];
-    
+
     collection.getAll().forEach(item => {
-        if(item.inputPath.includes(FolderName)) {
-          item.outputPath = item.outputPath.replace(`/${FolderName}`,'');;
-          item.url = item.url.replace(`/${FolderName}`,'');
-          item.data.page.url = item.url;
+      if (item.inputPath.includes(FolderName)) {
+        item.outputPath = item.outputPath.replace(`/${FolderName}`, '');;
+        item.url = item.url.replace(`/${FolderName}`, '');
+        item.data.page.url = item.url;
 
-          //content pulled in from JSON
-          const jsonData = item.data.data;
-          item.data.layout = "page";
-          item.data.tags = ['news'];
-          item.data.title = jsonData.title;
-          item.data.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
-          item.data.meta = jsonData.excerpt;
-          item.data.description = jsonData.excerpt;
-          item.data.lead = jsonData.excerpt;
-          item.data.author = jsonData.author;
+        //content pulled in from JSON
+        const jsonData = item.data.data;
+        item.data.layout = "page";
+        item.data.tags = ['news'];
+        item.data.title = jsonData.title;
+        item.data.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
+        item.data.meta = jsonData.excerpt;
+        item.data.description = jsonData.excerpt;
+        item.data.lead = jsonData.excerpt;
+        item.data.author = jsonData.author;
 
-          if(jsonData.media) {
-            jsonData.media.filter(x=>x.source_url_match).forEach(m=>{
-              replaceContent(item,new RegExp(m.source_url,'g'),'/'+wordpressImagePath+'/'+m.path);
-            });
+        if (jsonData.media) {
+          const featuredMedia = jsonData.media.find(x => x.featured);
+          if (featuredMedia) {
+            item.data.previewimage = wordpressImagePath + '/' + featuredMedia.path;
           }
-        };
+
+          jsonData.media.filter(x => x.source_url_match).forEach(m => {
+            replaceContent(item, new RegExp(m.source_url, 'g'), '/' + wordpressImagePath + '/' + m.path);
+          });
+        }
+      };
     });
 
     return output;
@@ -54,14 +59,14 @@ module.exports = function(eleventyConfig) {
 
 
 
-  eleventyConfig.addCollection("mySort", function(collection) {
+  eleventyConfig.addCollection("mySort", function (collection) {
     let posts = [];
-    collection.getAll().forEach( (item) => {
-      if(item.data.tags && item.data.tags[0] == 'news') {
+    collection.getAll().forEach((item) => {
+      if (item.data.tags && item.data.tags[0] == 'news') {
         posts.push(item);
       }
     })
-    return posts.sort(function(a, b) {
+    return posts.sort(function (a, b) {
       return new Date(a.data.publishdate) - new Date(b.data.publishdate);
     }).reverse();
   });
