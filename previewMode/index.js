1140cedb783cfd5cf94ea2bab4d07af3b30fa6b4
Carter Medlin <carterm@users.noreply.github.com>
2021-09-21 12:20:07 -0700

    removing unused reference

--- previewMode/index.js
+++ previewMode/index.js
@@ -1,6 +1,6 @@
 //Using Azure FaaS, the service can render a single page from remote content, while redirecting all other resource requests (.css, .png, etc) back to the real web server.
 
-const { azureFunctionHandler, serverlessHandler } = require("@cagov/11ty-serverless-preview-mode");
+const { azureFunctionHandler } = require("@cagov/11ty-serverless-preview-mode");
 const fetch = require('node-fetch/lib');
 
 /**
