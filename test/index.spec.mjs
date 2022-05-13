f01452ba21890486fc02e5ca563f09a7d3d63dba
Aaron Hans <aaronhans@gmail.com>
2022-05-13 14:48:48 -0700

    fix duplicate ids, run tests on mobile and desktop sizes

--- test/index.spec.mjs
+++ test/index.spec.mjs
@@ -10,6 +10,24 @@ let pageUrls = ["/", "/who-we-are/our-vision/", "/who-we-are/how-we-work/", "/wh
 
 pageUrls.forEach(pageUrl => {
 
+  test("desktop: a11y page tests "+pageUrl, async ({ page }) => {
+    // use default viewport size which is desktop
+
+    await page.goto(testLocation+pageUrl);
+  
+    await injectAxe(page);
+
+    await checkA11y(page, null, {
+      detailedReport: true,
+      detailedReportOptions: { html: true },
+    })
+  
+  });
+});
+
+// run same tests on mobile too
+pageUrls.forEach(pageUrl => {
+
   test("mobile: a11y page tests "+pageUrl, async ({ page }) => {
     await page.setViewportSize({
       width: 360,
