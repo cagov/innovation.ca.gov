12d85a286276364f4dde30bcb9d770012d0c41fc
Aaron Hans <aaronhans@gmail.com>
2022-05-13 15:46:37 -0700

    comment out unused 7 minute tabs for now

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '@cagov/ds-site-navigation';
 import '@cagov/ds-page-feedback';
 import '@cagov/ds-page-navigation';
-import '@zachleat/seven-minute-tabs';
-import { augmentSevenMinuteTabs } from './seven-minute-tabs-augment.js';
-window.addEventListener('load',augmentSevenMinuteTabs);
+// import '@zachleat/seven-minute-tabs';
+// import { augmentSevenMinuteTabs } from './seven-minute-tabs-augment.js';
+// window.addEventListener('load',augmentSevenMinuteTabs);
