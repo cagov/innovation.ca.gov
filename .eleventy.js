c81705f41cd68b49cc6a123bc36742343774caac
Aaron Hans <aaronhans@gmail.com>
2022-05-19 12:35:20 -0700

    build fix

--- .eleventy.js
+++ .eleventy.js
@@ -40,7 +40,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy({ "wordpress/media": wordpressImagePath });
-  eleventyConfig.addPassthroughCopy({ "wordpress/media/2022/05": "/wp-content/uploads/2022/05" });
+  eleventyConfig.addPassthroughCopy({ "wordpress/media/2022/05": "wp-content/uploads/2022/05" });
   eleventyConfig.addPassthroughCopy({ "src/img": "img" });
   eleventyConfig.addPassthroughCopy({ "src/pdf": "pdf" });
   eleventyConfig.addPassthroughCopy({ "src/css/fonts": "fonts" });
