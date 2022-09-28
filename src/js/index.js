c6f83308fa3f4a349661fe63c75ffd773fd44840
Aaron Hans <aaronhans@gmail.com>
2022-09-28 15:03:58 -0700

    update page to match Peggy updates

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,12 @@ import './ga-tracking.js';
 // import '@zachleat/seven-minute-tabs';
 // import { augmentSevenMinuteTabs } from './seven-minute-tabs-augment.js';
 // window.addEventListener('load',augmentSevenMinuteTabs);
+
+// this is only used on the test thank-you-for-applying page
+export class CaGovDateTime extends window.HTMLElement {
+  connectedCallback() {
+    let today = new Date();
+    this.innerHTML = `${today.toLocaleDateString()} ${today.toLocaleTimeString()}`;
+  }
+}
+window.customElements.define('cagov-date-time', CaGovDateTime);
