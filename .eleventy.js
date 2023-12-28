426fc1952e61d5d076c3c5c22c7d0c550070062f
Jon Jensen <github@id.xjensen.com>
2023-12-28 15:18:45 -0800

    Build fix progress

--- .eleventy.js
+++ .eleventy.js
@@ -122,9 +122,15 @@ module.exports = function (eleventyConfig) {
         item.data.tags = jsonData.tags;
 
         // WordPress will lazy load everything and you have to put php code in your theme to override it. We don't want to lazy load our featuref image
-        item.template.frontMatter.content = item.template.frontMatter.content.replace('loading="lazy" class="cagov-featured-image','class="cagov-featured-image');
+        //item.template.frontMatter.content = item.template.frontMatter.content.replace('loading="lazy" class="cagov-featured-image','class="cagov-featured-image');
 
-        let html = item.template.frontMatter.content;
+        let html = await item.template.read().then(t => {
+          return t.content
+        });
+
+        // WordPress will lazy load everything and you have to put php code in your theme to override it. We don't want to lazy load our featuref image
+        html = html.replace('loading="lazy" class="cagov-featured-image','class="cagov-featured-image');
+        
         // Temporary rewrite of feature card h2 tag to h1. This is fixed in newer version of Gutenberg blocks plugin still under review so can be deleted when this no longer comes through as h2 causing a11y test failures
         if (html.indexOf('cagov-featured-sidebar') > -1) {
           const {
