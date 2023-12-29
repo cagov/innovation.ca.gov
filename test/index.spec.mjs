1c3e9da176bb2664034b508b9aeadf4116d7ef5e
Jon Jensen <github@id.xjensen.com>
2023-12-29 14:08:53 -0800

    Change how we make allFiles.json

--- test/index.spec.mjs
+++ test/index.spec.mjs
@@ -1,42 +1,26 @@
-import fs from 'fs';
+import fs from "fs";
 import { test, expect } from "@playwright/test";
-import {
-  injectAxe,
-  checkA11y
-} from "axe-playwright";
+import { injectAxe, checkA11y } from "axe-playwright";
 
 let testLocation = "http://localhost:8080";
 
-let pageUrls = ["/"];
+let pageList = JSON.parse(fs.readFileSync("./_site_dist/allFiles.json"));
+let pageUrls = pageList.map((page) => page.url);
 
-
-let pageList = JSON.parse(fs.readFileSync('./_site_dist/allFiles.json'));
-pageList.forEach(page => {
-  let outputUrl = page.outputPath.replace('_site/','/').replace('/index.html','/');
-  if(pageUrls.indexOf(outputUrl) === -1) {
-    pageUrls.push(outputUrl);
-  }
-})
-
-
-
-pageUrls.forEach(pageUrl => {
-  console.log('testing '+pageUrl)
-
-  test("desktop: a11y page tests "+pageUrl, async ({ page }) => {
+pageUrls.forEach((pageUrl) => {
+  test(`desktop: a11y page tests: ${pageUrl}`, async ({ page }) => {
     // use default viewport size which is desktop
+    await page.goto(testLocation + pageUrl);
 
-    await page.goto(testLocation+pageUrl);
-  
     await injectAxe(page);
 
     await checkA11y(page, null, {
       detailedReport: true,
       detailedReportOptions: { html: true },
-    })
-  
+    });
   });
 });
+
 /*
 
 // run same tests on mobile too
