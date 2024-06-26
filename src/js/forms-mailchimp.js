8730ad1873361022800de17f274f5bbaf0d55f8e
Zakiya <zakiya@users.noreply.github.com>
2024-06-25 19:52:13 -0700

    Modify CalData form with lastest advice from UX team.

--- src/js/forms-mailchimp.js
+++ src/js/forms-mailchimp.js
@@ -5,33 +5,14 @@ if (mailchimpWrapper.length > 0) {
 
     // Define elements.
     const mailchimpSubmit = mailchimpWrapper[0].querySelectorAll('#mc-embedded-subscribe');
-    const mailchimpTextInputs = mailchimpWrapper[0].querySelectorAll('input.required')
+    const mailchimpTextInputs = mailchimpWrapper[0].querySelectorAll('input.required');
 
-    // Start by disabling the submit button.
-    mailchimpSubmit[0].classList.add('btn-disabled');
-
-    // Create counter for required inputs.
-    let inputsWithValues = 0;
-
-    // Once all items are filled in, enable the submit button.
-    mailchimpTextInputs.forEach((input) => {
-
-        // Listen for keystrokes.
-        input.addEventListener('keyup', () => {
-
-            // If the input has a value count it.
-            mailchimpTextInputs.forEach((input) => {
-                inputsWithValues += (input.value !== '') ? 1 : 0;
-            });
-
-            // If all inputs have values, enable.
-            if (inputsWithValues == mailchimpTextInputs.length) {
-                mailchimpSubmit[0].classList.remove('btn-disabled')
-            }
-
-            // Reset the counter.
-            inputsWithValues = 0;
+    // Check for required fields on click.
+    mailchimpSubmit[0].addEventListener("click", () => {
+        mailchimpTextInputs.forEach((input) => {
+            input.value === '' ? input.classList.add('show') :
+                input.classList.remove('show')
         });
     });
 }
-;
+
