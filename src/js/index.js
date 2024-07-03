e02682d6fdcd00a6b66e8d1050311604eccad644
Zakiya <zakiya@users.noreply.github.com>
2024-07-03 07:46:30 -0700

    Removing custom mail chimp form

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,3 @@ import '@cagov/ds-page-feedback';
 import '@cagov/ds-page-navigation';
 import './cagov-connector-lines.js';
 import './ga-tracking.js';
-import './forms-mailchimp';
-
-
-// import '@zachleat/seven-minute-tabs';
-// import { augmentSevenMinuteTabs } from './seven-minute-tabs-augment.js';
-// window.addEventListener('load',augmentSevenMinuteTabs);
