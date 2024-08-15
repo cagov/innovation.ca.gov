2cb7e003966e3b8d2c5f5393026311f7070bc70f
Jim Bumgardner <jbum@jbum.com>
2024-08-14 17:06:12 -0700

    Got paging working. In progress.  Preview at projects-test.

--- src/js/cagov-paginator.js
+++ src/js/cagov-paginator.js
@@ -15,6 +15,7 @@ class CAGovPaginator extends HTMLElement {
         console.log("number blocks",this.pagedBlocks.length);
         this.nbrPages = Math.ceil(this.pagedBlocks.length / this.perPage);
         this.currentPage = 1; // !!! pull from URL
+        this.boundPaginationHandler = this.paginationHandler.bind(this);
         console.log("nbrPages",this.nbrPages);
 
         if(document.readyState === 'complete') {
@@ -31,10 +32,47 @@ class CAGovPaginator extends HTMLElement {
         // this.pagedContainer = document.querySelector(this.pagedContainerSelector);
         // this.pagedBlocks = this.pagedContainer.querySelectorAll(this.pagedBlockSelector);
         // console.log("number blocks",this.pagedBlocks.length);
+        console.log("drawPaginator");
 
 
         var markup = `<cagov-pagination data-current-page="${this.currentPage}" data-total-pages="${this.nbrPages}"></cagov-pagination>`;
         this.innerHTML = markup;
+        if (this.querySelector("cagov-pagination")) {
+            var paginator = this.querySelector("cagov-pagination");
+            console.log("binding cagov-pagination listener");
+            paginator.removeEventListener(
+              "paginationClick",
+              this.boundPaginationHandler,
+              true
+            );
+            paginator.addEventListener(
+                "paginationClick",
+                this.boundPaginationHandler,
+                true
+              );
+            this.drawCurrentPage();
+        }
+        else {
+            console.log("cagov-pagination not found");
+        }
+      
+
+    }
+
+    drawCurrentPage() {
+        console.log("drawCurrentPage",this.currentPage);
+        this.pagedContainer.innerHTML = "";
+        this.pagedBlocks.forEach((block,index) => {
+            if (index >= (this.currentPage - 1) * this.perPage && index < this.currentPage * this.perPage) {
+                this.pagedContainer.appendChild(block);
+            }
+        });
+    }
+
+    paginationHandler(e) {
+        console.log("paginationHandler called", e);
+        this.currentPage = parseInt(e.detail);
+        this.drawCurrentPage();
     }
 }
 
